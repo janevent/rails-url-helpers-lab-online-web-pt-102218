4aea44140cfcadeb7694a185b099ec80aa4d0b0f
Jane Venter <janeanneventer@gmail.com>
2019-03-31 21:37:26 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   
   get 'students', to: 'students#index'
   
-  get 'students/:id', to: 'students#show'
+  get 'student/:id', to: 'students#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
